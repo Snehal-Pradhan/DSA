5d82566a4ea0ebfd214550862d80a98824ad65fc
Snehal Pradhan <snehalpradhan23@gmail.com>
2024-10-16 16:40:40 +0000

    started

--- 01.js
+++ 01.js
@@ -2,8 +2,13 @@ const grocery = ["milk","sugar","dahi","peanuts","banana"];
 
 const searchForItem = (item,array) => {
     for(let i =0;i<array.length;i++){
-        if(array[i]===item)console.log(`found ${item}`)
+        if(array[i]===item){
+             console.log(`found ${item}`)
             return
+        }
+           
     }
     return console.log("not found");
 }
+
+searchForItem("banana",grocery)
